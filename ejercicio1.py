052dab7f71ddf1a638f8a3c0de13dbb615945f4c
mattiasrortega08 <mattiasrmo@gmail.com>
2025-04-01 11:21:18 -0600

    Ejemplo 1 Calcular Temperatura

---
+++ ejercicio1.py
@@ -0,0 +1,4 @@
+temp_fah = float(input("Ingrese la temperatura en Fahrenheit: "))
+temp_Cel = (temp_fah - 32) * 5 / 9
+print(f"Grados Celsius {temp_Cel:.2f}")       
+print(f"Grados Kelvin {temp_cel:.2f}")
