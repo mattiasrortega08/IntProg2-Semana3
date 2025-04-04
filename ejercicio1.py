3e5eaeef78a131d080484cf80fabdd034e0d0f4e
mattiasrortega08 <mattiasrmo@gmail.com>
2025-04-03 20:14:49 -0600

    Delete ejercicio1.py

--- ejercicio1.py
+++
@@ -1,5 +0,0 @@
-temp_fah = float(input("Ingrese la temperatura en F: "))
-temp_cel = (temp_fah - 32) * 5 / 9
-temp_kel = temp_cel + 273.15
-print(f"Grados Celsius {temp_Cel:.2f}")       
-print(f"Grados Kelvin {temp_cel:.2f}")
